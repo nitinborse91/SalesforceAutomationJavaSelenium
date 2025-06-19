51e5046ad09d69a321d370f6b857ac85172008fd
nitinborse91 <nitin.tester1991@gmail.com>
2025-06-19 22:48:46 +0530

    first commit to put Project of GitHub repo

---
+++ src/main/java/TestCases/SFLoginPage.java
@@ -0,0 +1,15 @@
+package TestCases;
+
+public class SFLoginPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is a test case for Salesforce Login Page.");
+		System.out.println("Start the Salesforce Automation Testing.");
+		
+		//Create WebDriver instance and initialize it
+		
+		
+	}
+
+}
